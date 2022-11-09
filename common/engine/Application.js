f26827207735588a14249eb36765a295b08bc06e
parch99 <72417373+parch99@users.noreply.github.com>
2022-11-09 19:34:04 +0100

    init

---
+++ common/engine/Application.js
@@ -0,0 +1,70 @@
+export class Application {
+    
+    constructor(canvas, glOptions) {
+        this._update = this._update.bind(this);
+        console.log("in Application/engine construct");
+        this.canvas = canvas;
+        this._initGL(glOptions);
+    }
+
+    async init() {
+        console.log("in Application/engine init() before await this.start");
+        await this.start();
+        console.log("in Application/engine init() after await this.start");
+        requestAnimationFrame(this._update);
+    }
+
+    _initGL(glOptions) {
+        this.gl = null;
+        try {
+            this.gl = this.canvas.getContext('webgl2', glOptions);
+        } catch (error) {
+        }
+
+        if (!this.gl) {
+            console.log('Cannot create WebGL 2.0 context');
+        }
+    }
+
+    _update() {
+        this._resize();
+        this.update();
+        this.render();
+        requestAnimationFrame(this._update);
+    }
+
+    _resize() {
+        const canvas = this.canvas;
+        const gl = this.gl;
+
+        const pixelRatio = window.devicePixelRatio;
+        const width = pixelRatio * canvas.clientWidth;
+        const height = pixelRatio * canvas.clientHeight;
+
+        if (canvas.width !== width || canvas.height !== height) {
+            canvas.width = width;
+            canvas.height = height;
+
+            gl.viewport(0, 0, gl.drawingBufferWidth, gl.drawingBufferHeight);
+
+            this.resize();
+        }
+    }
+
+    start() {
+        // initialization code (including event handler binding)
+    }
+
+    update() {
+        // update code (input, animations, AI ...)
+    }
+
+    render() {
+        // render code (gl API calls)
+    }
+
+    resize() {
+        // resize code (e.g. update projection matrix)
+    }
+
+}
